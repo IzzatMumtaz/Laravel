f046aaaa7ceac69c3830bbfb9ac49a55993ed897
kitabertiga <TugasPertugasan123@gmail.com>
2024-02-02 09:36:56 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>IZZAT MUMTAZ </h1>
+        <a href="pendataan">pendataan</a>
+        <a href="hitung">hitung</a>
+        <a href="daftar">daftar</a>
+    </body>
+</html>
